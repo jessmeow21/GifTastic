7ba54c970867cb2cc979748b14649ad15b3109a3
Jessica <jessica.sulaiman86@gmail.com>
2019-06-20 14:42:39 -0400

    response success

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//
+//gftastic js
+
+//connect to giphy api
+//ajax call and report response
+//make buttons for html 
+//input form for submissions and submit button
+
+// $("button").on("click", function () {
+
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=OAxCpgRbH01y0d1oR0AskyTeNu6pcWk3&q=&limit=10";
+// apikey = "OAxCpgRbH01y0d1oR0AskyTeNu6pcWk3",
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    //response successful
+    console.log(response);
+
+
+
+
+
+
+
+
+
+});
+
+// })
