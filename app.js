024089f1b7b969f9400ff2d47e4f2308a233098b
Jessica <jessica.sulaiman86@gmail.com>
2019-06-23 08:50:22 -0400

    working response

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
-//gftastic js
+/////////////////////giftastic
 
 //connect to giphy api
 //ajax call and report response
 //make buttons for html 
 //input form for submissions and submit button
-
-// $("button").on("click", function () {
-
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=OAxCpgRbH01y0d1oR0AskyTeNu6pcWk3&q=&limit=10";
-// apikey = "OAxCpgRbH01y0d1oR0AskyTeNu6pcWk3",
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    //response successful
-    console.log(response);
+//topics array
+//q, limit, and rating parameters 
 
 
+//onclick function
+$("#button").on("click", function () {
+    var search = $(this).attr("cat-button");
+
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        search + "&api_key=C6BXFZeCbeNMXSq2uaCAbt1f23BF0Qx6&limit=10";
+    //ajax call from giphy
+    $.ajax({
+        // url: "https://api.giphy.com/v1/gifs/trending?api_key=OAxCpgRbH01y0d1oR0AskyTeNu6pcWk3",
+        url: queryURL,
+        method: "GET"
+        // After the data from the AJAX request comes back
+
+    }).then(function (response) {
+        console.log(response);
 
 
 
 
-
+    });
 
 
 });
 
-// })
+
+
