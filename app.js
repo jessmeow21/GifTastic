3725826a68741a65530cb6808ec6f6e1fcd8a607
Jessica <jessica.sulaiman86@gmail.com>
2019-06-24 17:39:45 -0400

    work in progress with buttons

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
-/////////////////////giftastic
+// /////////////////////giftastic
 
-//connect to giphy api
-//ajax call and report response
-//make buttons for html 
-//input form for submissions and submit button
-//topics array
-//q, limit, and rating parameters 
-
-
-//onclick function
-$("#button").on("click", function () {
-    var search = $(this).attr("cat-button");
+// //connect to giphy api
+// //ajax call and report response
+// //make buttons for html 
+// //input form for submissions and submit button
+// //topics array
+// //q, limit, and rating parameters 
+// $(document).ready(function () {
 
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        search + "&api_key=C6BXFZeCbeNMXSq2uaCAbt1f23BF0Qx6&limit=10";
-    //ajax call from giphy
-    $.ajax({
-        // url: "https://api.giphy.com/v1/gifs/trending?api_key=OAxCpgRbH01y0d1oR0AskyTeNu6pcWk3",
-        url: queryURL,
-        method: "GET"
-        // After the data from the AJAX request comes back
+//     //pulls 10 images from giphy
+//     //click to start and stop gif 
+//     //array of topics, loop over array and created buttons for them each
+//     //function to add submissions from form to the array
+//     var carModels = ["cars", "toyota", "bmw", "batmobile", "honda"];
+//     //take topics in array and create buttons in html
+//     //by using a loop to append a button for each string in the array
 
-    }).then(function (response) {
-        console.log(response);
+//     //function to make buttons and add to page
+//     function carButtons(arrayToUse, classToAdd, areaToAddTo) {
+//         // $(areaToAddTo).empty();
+
+//         for (var i = 0; i < arrayToUse.length; i++) {
+//             console.log(arrayToUse);
+//             var carButton = $("<button>");
+//             carButton.addClass(classToAdd);
+//             carButton.attr("data-type", arrayToUse[i]);
+//             carButton.text(arrayToUse[i]);
+//             $(areaToAddTo).append(carButton);
+//         }
+//     }
+//     carButtons();
 
 
 
+// $(document).on("click", function () {
+//     var x = $(this).data("search");
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x +
+//         "&api_key=OAxCpgRbH01y0d1oR0AskyTeNu6pcWk3&limit=10";
+//     //ajax call from giphy
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//         // After the data from the AJAX request comes back
+//     }).then(function (response) {
+//         for (let i = 0; i < response.data.length; i++) {
+//             $("#gif-area").prepend("<p>Rating: " + response.data[i].rating + "</p>");
+//             //append image
+//             $("#gif-area").prepend("<img src=' " + response.data[i].images.downsized.url + " '>");
+//         }
+//     })
+// })
 
-    });
 
-
-});
-
+// });
 
 
